fffa057b1d1d8447a60b580d6e48a60f58863f8c
Gaurav Narkhede <narkhedegs@gmail.com>
2014-11-03 15:57:42 -0500

    Refactored Environment object.

--- RestApiTester.Common/Environment.cs
+++ RestApiTester.Common/Environment.cs
@@ -4,6 +4,11 @@ namespace RestApiTester.Common
 {
     public class Environment
     {
+        public Environment()
+        {
+            Variables = new Dictionary<string, string>();
+        }
+
         public string Name { get; set; }
         public IDictionary<string,string> Variables { get; set; }
     }
